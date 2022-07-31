5179358e143953f1487ca356fddfc3a63f2afcbd
Brian Pelaez <pelaez_brian@hotmail.com>
2022-07-30 22:29:45 -0300

    Se agrega  mock

---
+++ src/app/mock-task.ts
@@ -0,0 +1,34 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Terminar segundo modulo de Angular",
+        day: 'Septiembre 7 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Terminar tercer modulo de Angular",
+        day: 'Noviembre 5 a las 11:00',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Sacar al perro",
+        day: 'Agosto 22 a las 10:00',
+        reminder: true
+    },
+    {
+        id: 5,
+        text: "Ir al gym",
+        day: 'Agosto 5 a las 15:00',
+        reminder: true
+    },
+]
